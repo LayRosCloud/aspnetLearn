f027c679b20bbebc57b230dd35bdae9250498b49
layroscloud <vogistv@gmail.com>
2022-10-25 11:32:57 +0700

    Add pizza pages, images and other

---
+++ SiteASPMVC/Controllers/PizzasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteASPMVC.Models;
+
+namespace SiteASPMVC.Controllers
+{
+    public class PizzasController : Controller
+    {
+        public static PizzaViewModel Pizza { get; set; } = new PizzaViewModel("хуй", "пизда", 2);
+        public PizzasController()
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View(Pizza);
+        }
+        public IActionResult CustomPizza()
+        {
+            return View(Pizza);
+        }
+    }
+}
---
+++ SiteASPMVC/Models/PizzaViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SiteASPMVC.Models
+{
+    public class PizzaViewModel
+    {
+        public string ImageTitle { get; set; }
+        public string Title { get; set; }
+        public float BasePrice { get; set; } = 2f;
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+        public bool Tuna { get; set; }
+        public bool Beef { get; set; }
+        public bool Ham { get; set; }
+        public PizzaViewModel(string image, string title, float price)
+        {
+            ImageTitle = image;
+            Title = title;
+            BasePrice = price;
+        }
+        public PizzaViewModel()
+        {
+
+        }
+    }
+}
